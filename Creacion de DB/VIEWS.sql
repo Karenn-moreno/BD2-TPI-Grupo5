30b5ec2ec35f9e42a23804db9c84339ee4ce9412
Manuel Borsani <manuborsa01@hotmail.com>
2025-11-03 20:06:59 -0300

    Agrega VISTAS de estrenos

--- Creacion de DB/VIEWS.sql
+++ Creacion de DB/VIEWS.sql
@@ -1,4 +1,4 @@
----Comentario de prueba
+
 ---vista_FuncionesDisponibles : lista de todas las funciones disponibles por pelicula, sala y horario
 
 CREATE VIEW vista_FuncionesDisponibles
@@ -23,3 +23,35 @@ INNER JOIN
 WHERE 
     s.EstadoSala = 1            -- salas activas
     AND f.FechaYHora >= GETDATE() -- funciones que aun no han comenzado
+
+---vista_Estrenos : lista las funciones estrenadas en los últimos 14 días. Muestra día y horario de la función, titulo de la película, duración y género.
+
+
+CREATE VIEW vista_UltimosEstrenos 
+AS
+SELECT
+	p.Titulo, p.Genero, p.Duracion, CAST(f.FechaYHora AS date) AS Dia, CAST(f.FechaYHora AS TIME) AS Horario
+FROM
+	Funcion AS f
+INNER JOIN
+	Pelicula AS p ON f.idPelicula = p.id
+WHERE
+	f.FechaYHora >= DATEADD(day, -14, GETDATE())
+	AND
+	f.FechaYHora <= GETDATE()
+
+---vista_EstrenosProximos : lista las funciones a estrenarse en los próximos 7 días. Muestra día y horario de la función, titulo de la película, duración y género.
+	
+CREATE VIEW vista_EstrenosProximos 
+AS
+SELECT
+	p.Titulo, p.Genero, p.Duracion, CAST(f.FechaYHora AS date) AS Dia, CAST(f.FechaYHora AS TIME) AS Horario
+FROM
+	Funcion AS f
+INNER JOIN
+	Pelicula AS p ON f.idPelicula = p.id
+WHERE
+	f.FechaYHora <= DATEADD(day, 7, GETDATE())
+	AND
+	f.FechaYHora >= GETDATE()
+
