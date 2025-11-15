287e20dcdb53af78043a226db889a75668b5aaec
kmoreno <morenokarenanahi24@gmail.com>
2025-11-15 18:34:08 -0300

    agrega procedimiento Reprogramar funcion

--- Creacion de DB/PROCEDIMIENTOSALMACENADOS.sql
+++ Creacion de DB/PROCEDIMIENTOSALMACENADOS.sql
@@ -42,3 +42,101 @@ BEGIN
 GROUP BY p.Titulo, P.Duracion, p.Clasificacion, p.Genero
 END
 ---EXEC sp_EntradasPorPelícula 10
+
+
+--realiza las rutinas validas para reprogramar una funcion
+CREATE PROCEDURE sp_ReprogramarFuncion
+    @idFuncion INT,          
+    @NuevaFechaYHora DATETIME2, 
+    @NuevoIdSala INT         
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        -- validar que la función y la sala existan
+        IF NOT EXISTS (SELECT 1 FROM FUNCION WHERE id = @idFuncion)
+        BEGIN
+            RAISERROR('Error: La funcion con ID %d no existe.', 16, 1, @idFuncion);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        IF NOT EXISTS (SELECT 1 FROM SALA WHERE id = @NuevoIdSala)
+        BEGIN
+            RAISERROR('Error: La sala con ID %d no existe.', 16, 1, @NuevoIdSala);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        -- validacion de entradas vendidas
+        DECLARE @SalaActual INT;
+        SELECT @SalaActual = idSala FROM FUNCION WHERE id = @idFuncion;
+
+        -- si la sala está cambiando y hay entradas vendidas...
+        IF (@SalaActual <> @NuevoIdSala) AND EXISTS (SELECT 1 FROM ENTRADA WHERE idFuncion = @idFuncion)
+        BEGIN
+            RAISERROR('Error: No se puede cambiar de sala una funcion que ya tiene entradas vendidas. Esto viola la integridad de los asientos (Trigger TR_AsientoSala). Primero debe anular las ventas asociadas.',16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        --validar conflictos de horario
+        DECLARE @MiNuevoInicio DATETIME2 = @NuevaFechaYHora;
+        DECLARE @MiDuracion INT;
+        DECLARE @MiNuevoFin DATETIME2;
+
+        -- Obtener la duración de la película de esta función
+        SELECT @MiDuracion = p.Duracion 
+        FROM PELICULA p
+        JOIN FUNCION f ON p.id = f.idPelicula
+        WHERE f.id = @idFuncion;
+
+        -- calcular la hora de fin de nuestra función
+        SET @MiNuevoFin = DATEADD(minute, @MiDuracion, @MiNuevoInicio);
+
+        -- Buscamos si OTRA función se superponeen 
+        IF EXISTS (
+            SELECT 1
+            FROM FUNCION f
+            JOIN PELICULA p ON f.idPelicula = p.id
+            WHERE 
+                f.idSala = @NuevoIdSala      
+            AND f.id <> @idFuncion           
+            AND @MiNuevoInicio < DATEADD(minute, p.Duracion, f.FechaYHora) 
+            AND f.FechaYHora < @MiNuevoFin 
+        )
+        BEGIN
+            RAISERROR('Error de conflicto: La sala %d ya esta ocupada en ese horario.', 16, 1, @NuevoIdSala);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+        -- actualizamos la función
+        UPDATE FUNCION
+        SET 
+            FechaYHora = @NuevaFechaYHora,
+            idSala = @NuevoIdSala
+        WHERE 
+            id = @idFuncion;
+
+        COMMIT TRANSACTION;
+
+        PRINT 'La funcion ID ' + CAST(@idFuncion AS VARCHAR) + ' ha sido reprogramada exitosamente.';
+    END TRY
+    BEGIN CATCH
+        --si hay falla ,deshacemos todo
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+        -- Re-lanzamos el error original 
+        PRINT 'Ocurrio un error. Se revirtio la operacion.';
+        THROW; 
+    END CATCH
+END;
+GO
+
+--Probar conflicto de horario
+/*EXEC sp_ReprogramarFuncion 
+    @idFuncion = 3,
+    @NuevaFechaYHora = '2025-11-14 21:30:00', -- superpone
+    @NuevoIdSala = 3;
+*/
